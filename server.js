5ecee0355b325c675a571985ca95213d7dd33438
arindam <arindamlahiri17@gmail.com>
2020-12-07 22:04:09 +0530

    adding cors support

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const { MongoClient } = require('mongodb')
 const nodemailer = require('nodemailer')
+const cors = require('cors')
 require('dotenv').config()
 
 //To parse req.body
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+//To enable cors
+app.use(cors())
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/fundraisers', require('./routes/fundraisers'))
