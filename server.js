61c12e9ed2a344a8413b75e8178f192083de82c5
arindam <arindamlahiri17@gmail.com>
2020-12-15 03:08:09 +0530

    changes for heroku deployment

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.use('/api/razorpay', require('./routes/razorpay'))
 app.use('/api/admin', require('./routes/admin'))
 app.use('/verify', require('./routes/verify'))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.setHeader('X-Frame-Options', 'DENY')
+    res.setHeader('Content-Security-Policy', "frame-ancestors 'none'")
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`)
